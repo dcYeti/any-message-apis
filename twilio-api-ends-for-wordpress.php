0c941a7d17f98a90dc45421e47cb73772c7999c8
Anthony Ahn <jovia@iMac.local>
2022-04-03 19:37:22 -0400

    Initialize WP Boilerplate

---
+++ twilio-api-ends-for-wordpress.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Twilio API Ends for Wordpress
+Description: A wordpress plugin that sets up API Endpoints for you to use for your AJAX Front-ends
+Version:  1.0.0
+Author: Anthony Ahn - Antimony Software
+*/
+
+class TwilioApiEndsWP {
+
+}
+
+
+
+
